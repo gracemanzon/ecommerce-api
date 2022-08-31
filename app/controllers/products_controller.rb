49f634e938d3cd59ddc4d57c3d1415359716a717
grace manzon <grace.xws@gmail.com>
2022-08-31 15:32:24 -0400

    add post/create request and action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,4 +8,15 @@ class ProductsController < ApplicationController
     product = Product.find_by(id: params["id"])
     render json: product.as_json
   end
+
+  def create
+    product = Product.new(
+      name: "Rubber Date Stamp",
+      price: 18,
+      image_url: "https://i.etsystatic.com/29642716/r/il/e95cce/3217855779/il_570xN.3217855779_9ock.jpg",
+      description: "A set of wooden block stamps featuring assorted Months/Dates.",
+    )
+    product.save
+    render json: product.as_json
+  end
 end
