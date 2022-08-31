adde22ca00b6b29a3b45994c6460c4467457f144
grace manzon <grace.xws@gmail.com>
2022-08-31 13:49:14 -0400

    making the controller dynamic using params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,18 +4,18 @@ class ProductsController < ApplicationController
     render json: products.as_json
   end
 
-  def product_notebook
-    product = Product.find_by(id: 1)
+  def one_product
+    product = Product.find_by(id: params["id"])
     render json: product.as_json
   end
 
-  def product_pen
-    product = Product.find_by(id: 3)
-    render json: product.as_json
-  end
+  # def product_pen
+  #   product = Product.find_by(id: 3)
+  #   render json: product.as_json
+  # end
 
-  def product_printer
-    product = Product.find_by(id: 6)
-    render json: product.as_json
-  end
+  # def product_printer
+  #   product = Product.find_by(id: 6)
+  #   render json: product.as_json
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   get "/products", controller: "products", action: "product_all"
-  get "/product_notebook", controller: "products", action: "product_notebook"
-  get "/product_pen", controller: "products", action: "product_pen"
-  get "/product_printer", controller: "products", action: "product_printer"
+
+  get "/product_one/:id", controller: "products", action: "one_product"
+  # get "/product_pen", controller: "products", action: "product_pen"
+  # get "/product_printer", controller: "products", action: "product_printer"
+
   # alternate syntax -- get "/product_printer" => "products#product_printer"
 end
