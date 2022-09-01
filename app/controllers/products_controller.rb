0668aa657a569cf475bbb5deb7e6eecc487e38f4
grace manzon <grace.xws@gmail.com>
2022-09-01 12:25:58 -0400

    make a dynamic create method in 'products' controller and through 'request.http'

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,10 +11,10 @@ class ProductsController < ApplicationController
 
   def create
     product = Product.new(
-      name: "Rubber Date Stamp",
-      price: 18,
-      image_url: "https://i.etsystatic.com/29642716/r/il/e95cce/3217855779/il_570xN.3217855779_9ock.jpg",
-      description: "A set of wooden block stamps featuring assorted Months/Dates.",
+      name: params["name"],
+      price: params["price"],
+      image_url: params["url"],
+      description: params["description"],
     )
     product.save
     render json: product.as_json
