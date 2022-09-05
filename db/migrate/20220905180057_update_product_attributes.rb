28d2a419b3b562bef036a8570e62e8fedb216fb5
grace manzon <grace.xws@gmail.com>
2022-09-05 14:09:01 -0400

    add migration file to update products db structure - change price data type to decimal, add inventory data

---
+++ db/migrate/20220905180057_update_product_attributes.rb
@@ -0,0 +1,6 @@
+class UpdateProductAttributes < ActiveRecord::Migration[7.0]
+  def change
+    change_column :products, :price, :decimal, precision: 9, scale: 2
+    add_column :products, :inventory, :integer
+  end
+end
