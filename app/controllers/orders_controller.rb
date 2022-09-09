61315436269e9032fdeb3b0a681f00defcb4106b
grace manzon <86327895+gracemanzon@users.noreply.github.com>
2022-09-09 14:15:18 -0400

    alter authentication with before_action instead of if statements

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,34 +1,37 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user
+
   def create
-    if current_user
-      product = Product.find_by(id: params["product_id"])
-      subtotal = product.price * params["quantity"].to_i
-      tax = product.tax * params["quantity"].to_i
-      total = subtotal + tax
+    # if current_user
+    product = Product.find_by(id: params["product_id"])
+    subtotal = product.price * params["quantity"].to_i
+    tax = product.tax * params["quantity"].to_i
+    total = subtotal + tax
 
-      @order = Order.new(
-        user_id: current_user.id,
-        product_id: params["product_id"],
-        quantity: params["quantity"],
-        subtotal: subtotal,
-        tax: tax,
-        total: total,
-      )
+    @order = Order.new(
+      user_id: current_user.id,
+      product_id: params["product_id"],
+      quantity: params["quantity"],
+      subtotal: subtotal,
+      tax: tax,
+      total: total,
+    )
 
-      if @order.save
-        render template: "orders/show"
-      else
-        render json: { error: "order not saved properly" }
-      end
+    if @order.save
+      render template: "orders/show"
     else
-      render json: { message: "you must log in to create a new order" }, status: :unauthorized
+      render json: { error: "order not saved properly" }
     end
+    # else
+    #   render json: { message: "you must log in to create a new order" }, status: :unauthorized
+    # end
   end
 
   def show
     @order = Order.find_by(id: params["id"])
 
-    if current_user && current_user.id == @order.user_id
+    # if current_user && current_user.id == @order.user_id
+    if current_user.id == @order.user_id
       render template: "orders/show"
     else
       render json: { message: "you are not authorized to view this page" }, status: :unauthorized
@@ -36,11 +39,11 @@ class OrdersController < ApplicationController
   end
 
   def index
-    if current_user
-      @orders = current_user.orders
-      render template: "orders/index"
-    else
-      render json: { message: "please log in to view orders" }, status: :unauthorized
-    end
+    # if current_user
+    @orders = current_user.orders
+    render template: "orders/index"
+    # else
+    #  render json: { message: "please log in to view orders" }, status: :unauthorized
+    # end
   end
 end
