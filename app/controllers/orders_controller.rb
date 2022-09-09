497a7db96fafae231ec811298d8f4a9e835b20b2
grace manzon <86327895+gracemanzon@users.noreply.github.com>
2022-09-09 13:38:47 -0400

    AFTER joeys demo -- globalizes subtotal, tax and total

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,19 +1,21 @@
 class OrdersController < ApplicationController
   def create
     product = Product.find_by(id: params["product_id"])
-    tax = product.price * 0.08
+    subtotal = product.price * params["quantity"].to_i
+    tax = product.tax * params["quantity"].to_i
+    total = subtotal + tax
 
     order = Order.new(
       user_id: current_user.id,
       product_id: params["product_id"],
       quantity: params["quantity"],
-      subtotal: product.price,
+      subtotal: subtotal,
       tax: tax,
-      total: product.price + tax,
+      total: total,
     )
 
     if order.save
-      render json: { message: "order saved properly" }
+      render json: { message: "order saved properly", order: order.as_json }
     else
       render json: { error: "order not saved properly" }
     end
